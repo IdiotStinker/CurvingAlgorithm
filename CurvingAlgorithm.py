9665d6fa4ae7787bbd0a327d73461c75765b371d
IdiotStinker <saltylightning564@gmail.com>
2024-10-28 19:14:27 +0000

    Start Curving Algorithm

--- CurvingAlgorithm.py
+++ CurvingAlgorithm.py
@@ -1,1 +1,22 @@
+def denominatorCurving():
+    newGrades = []
+    ogDenominator = int(input("What is the original denominator? "))
+    newDenominator = int(input("What is the curved denominator? "))
+    for grade in grades:
+        newGrade = grade * ogDenominator / newDenominator
+        newGrade = str(newGrade)[:4]
+        newGrades.append(newGrade)
+    
+    return newGrades
 
+def percentToDecimal():
+    newGrades = []
+    for grade in grades:
+        grade /= 100
+        newGrades.append(grade)
+
+    grades = newGrades
+
+grades = [14/15, 10/15, 6/15, 10/15, 14/15, 13/15]
+
+print(denominatorCurving())
